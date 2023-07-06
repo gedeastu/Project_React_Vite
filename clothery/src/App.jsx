9b9ccbee745fba463daf2bdbe4ac7205f1401210
gedeastu <gede.astu@gmail.com>
2023-07-06 09:23:54 +0700

    up

--- clothery/src/App.jsx
+++ clothery/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-
+import Header from './components/header'
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <Header/>
     </>
   )
 }
 
-export default App
+export default App;
