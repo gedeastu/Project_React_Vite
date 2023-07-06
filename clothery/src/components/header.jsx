2fcc0169256fc32eb1b3108beed4ae60e3863a28
gedeastu <gede.astu@gmail.com>
2023-07-06 10:33:35 +0700

    up

--- clothery/src/components/header.jsx
+++ clothery/src/components/header.jsx
@@ -3,6 +3,7 @@ export default function Header(){
     return (
         <>
         <h1 className='text-blue-400'>Hello World</h1>
+        <button className='btn'>Login</button>
         </>
     )
 };
